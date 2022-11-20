9ac9722e51961ea39c97b23a84284d814fb3b38d
aejbarifar <aejbarifar@gmail.com>
2022-11-20 21:39:11 +0100

    increible

--- Plantilla_scripts_DDL_DML_Prog.sql
+++ Plantilla_scripts_DDL_DML_Prog.sql
@@ -1,27 +1,86 @@
--- Nombre alumnos/as:
--- Tema específico del proyecto:
--- Nº de etapa:
+-- Nombre alumnos/as: Anas Ejbari y Artius 
+-- Tema específico del proyecto: Bibliotecas-Ay.-Pamplona
+-- Nº de etapa: 2/Porgramación
 
 /*
 DISEÑO FÍSICO (RA2)
 A continuación, implementa en lenguaje SQL todas las sentencias necesarias para generar la BD. Las sentencias SQL, además de las tablas, deben incluir la creación de la BD.
 */
 
-CREATE DATABASE ...
-CREATE TABLE...
-SFSDFSD
+CREATE DATABASE Bibliotecas_Ay_Pamplona;
+
+use Bibliotecas_Ay_Pamplona;
+
+CREATE TABLE IF NOT EXISTS espacios(
+codigo INTEGER PRIMARY KEY,
+ubicacion VARCHAR(30) NOT NULL,
+aforo INTEGER NOT NULL,
+pizarra BOOLEAN NOT NULL,
+proyector BOOLEAN NOT NULL,
+megafonia BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS autor(
+codigo INT PRIMARY KEY,
+nombre VARCHAR(30) NOT NULL,
+apellidos VARCHAR(30),
+fecha_nacimiento DATE,
+fecha_muerte DATE,
+pais_origen VARCHAR(30),
+CONSTRAINT nombre_completo UNIQUE (nombre,apellidos)
+);
+
+
+
+
+
 /*
 LENGUAJE DE MANIPULACIÓN DE DATOS (RA3 y RA4)
 Este fichero debe tener todas las órdenes necesarias para realizar las operaciones de manipulación exigidas por la etapa de proyecto.*/
 
 /*INSERCIONES: Realiza todas las inserciones necesarias para que el resultado de las consultas arroje varios resultados, pero no todos los registros de una tabla.*/
 
-INSERT INTO...
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('1234', 'Sala 1', '15', '1', '1', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('3421', 'Sala 2', '12', '1', '1', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('2452', 'Sala 3', '13', '0', '0', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('6234', 'Sala 4', '11', '1', '1', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('7869', 'Sala 5', '15', '1', '1', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('4524', 'Sala 6', '11', '1', '1', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('1278', 'Sala 7', '7', '1', '0', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('6745', 'Sala 8', '12', '1', '1', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('7483', 'Sala 9', '17', '1', '1', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('9211', 'Sala 10', '13', '1', '1', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('6547', 'Sala 11', '19', '1', '1', '1');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('6854', 'Sala 12', '16', '1', '1', '0');
+INSERT INTO `bibliotecas_ay_pamplona`.`espacios` (`codigo`, `ubicacion`, `aforo`, `pizarra`, `proyector`, `megafonia`) VALUES ('1111', 'Sala 77', '7', '0', '0', '0');
 
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('1542', 'George', 'Orwell', '1903-07-25', '1950-01-21', 'India');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('1567', 'Antonio', 'Machado', '1875-08-26', '1939-02-22', 'España');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('6484', 'Miguel', 'De Cervantes', '1547-10-29', '1616-04-22', 'España');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('1685', 'George', 'Gordon Byron', '1788-01-22', '1824-04-19', 'Reino Unido');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('1574', 'William', 'Shakespeare', '1564-04-26', '1616-04-23', 'Reino Unido');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `pais_origen`) VALUES ('5838', 'George', 'Raymond Richard Martin', '1948-10-20', 'Estados Unidos');
+INSERT INTO `bibliotecas_ay_pamplona`.`autor` (`codigo`, `nombre`, `apellidos`, `fecha_nacimiento`, `fecha_muerte`, `pais_origen`) VALUES ('9876', 'Ray', 'Bradbury', '1920-09-22', '2012-06-05', 'Estados Unidos');
 /*CONSULTAS: Nota: Se incluirá como comentario el enunciado de cada consulta.*/
 
---Diseña una consulta para...
-SELECT...
+-- Diseña una consulta para...
+
+/*Ejercicio 2: Necesitamos saber cuál es el nº de espacios, aforo máximo y aforo medio
+de todos los espacios que tienen Pizarra y Proyector.*/
+ 
+ select count(*) as "nº Espacios", max(aforo) as "Aforo máximo" ,avg(aforo) as "Aforo medio" 
+ from espacios
+ where pizarra = true and proyector = true;
+
+/*¿Qué autores tienen más de 30 años y tienen un solo apellido? Ponlo en
+una sola columna usando la primera letra del nombre y el apellido. Ejemplo
+de formato: “Nombre: L. Dorado”.
+Pista: los apellidos pueden estar separadas o por un espacio o por un guion.*/
+SELECT CONCAT(LEFT(nombre,1),".", " ",apellidos) as "Nombre Completo"
+from autor
+where fecha_muerte - fecha_nacimiento > 30
+
+
 
 /*PROCEDIMIENTOS ALMACENADOS (RA5)
 Órdenes necesarias para implementar y ejecutar los procedimientos almacenados descritos en los enunciados.
