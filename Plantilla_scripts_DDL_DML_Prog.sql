dae6d814336efcb96b9d23a97734a2fe6b1528ac
adevorailu <adevorailu@gmail.com>
2022-11-23 14:06:47 +0100

    TODO CONSULTAS HECHO

--- Plantilla_scripts_DDL_DML_Prog.sql
+++ Plantilla_scripts_DDL_DML_Prog.sql
@@ -1,7 +1,7 @@
 -- Nombre alumnos/as: Anas Ejbari y Artius 
 -- Tema específico del proyecto: Bibliotecas-Ay.-Pamplona
 -- Nº de etapa: 2/Porgramación
-
+-- drop database bibliotecas_ay_pamplona;
 /*
 DISEÑO FÍSICO (RA2)
 A continuación, implementa en lenguaje SQL todas las sentencias necesarias para generar la BD. Las sentencias SQL, además de las tablas, deben incluir la creación de la BD.
@@ -19,7 +19,7 @@ pizarra BOOLEAN NOT NULL,
 proyector BOOLEAN NOT NULL,
 megafonia BOOLEAN NOT NULL
 );
-DROP TABLE espacios;
+
 
 CREATE TABLE IF NOT EXISTS prestamos(
 codigo INTEGER PRIMARY KEY,
@@ -51,7 +51,7 @@ titulo VARCHAR(50) NOT NULL,
 anyo YEAR NOT NULL,
 numero_edicion INT NOT NULL,
 genero ENUM("Acción", "Fantasía", "Ciencia y Ficción", "Amor") NOT NULL,
-descripcion VARCHAR(50)
+descripcion VARCHAR(200)
 );
 
 
@@ -110,15 +110,15 @@ INSERT INTO `bibliotecas_ay_pamplona`.`reservas` (`codigo`, `fecha_inicio`, `fec
 INSERT INTO `bibliotecas_ay_pamplona`.`reservas` (`codigo`, `fecha_inicio`, `fecha_fin`) VALUES ('3358', '2022-12-13 21:00:00', '2022-12-16 19:00:00');
 INSERT INTO `bibliotecas_ay_pamplona`.`reservas` (`codigo`, `fecha_inicio`, `fecha_fin`) VALUES ('9976', '2022-11-28 14:32:00', '2022-11-30 19:00:00');
 
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('1843538452953', 'Los 3 cerditos', '2009', '2', 'Fantasía', 'Aventuras de 3 cerdos a los que les persigue un lobo');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('4223162890371', 'Blancanieves', '2010', '5', 'Amor', 'Historia de amor entre una princesa y un principe');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('1414485901070', 'Cronicas de Narnia', '2014', '6', 'Accion', '4 hermanos atraviesan un armario magico que les indroduce a un mundo paralelo fantastico');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('9258972376260', 'Cars 2', '2017', '3', 'Acción', 'La historia de como Rayo McQueen consigue hacerse con la copa pistón');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('3900231913438', 'Los 5 justos otra vez', '2006', '1', 'Fantasía', '3 hermanos, su prima y un perro se reunen de nuevo en verano para vivir aventuras inimaginables');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('3340451941512', 'Regreso al futuro', '1999', '5', 'Ciencia y Ficción', 'Narra el proceso por el que pasa el protagonista para conseguir reparar la maquina que habia creado para viajar en el tiempo');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('5217974631252', 'Geronimo Stilton', '2011', '8', 'Fantasía', 'Cuenta las aventuras de los sueños de un raton, en los que se adentra en mundos magicos con todo tipo de criaturas');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('2660993476299', 'Codigo Lyoko', '2008', '3', 'Fantasia', 'Un grupo de amigos descubren un ordenador muy especial en el que encuentran un nuevo mundo virtual llamado Lyoko, ocupado por un virus y una joven de su edad');
-INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('2091566214299', 'Bajo la misma estrella', '2018', '11', 'Amor', 'Historia de amor en la que se hace incapie en la valoracion del tiempo de nuestra vida');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('18435384', 'Los 3 cerditos', '2009', '2', 'Fantasía', 'Aventuras de 3 cerdos a los que les persigue un lobo');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('42231628', 'Blancanieves', '2010', '5', 'Amor', 'Historia de amor entre una princesa y un principe');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('14144850', 'Cronicas de Narnia', '2014', '6', 'Accion', '4 hermanos atraviesan un armario magico que les indroduce a un mundo paralelo fantastico');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('92589720', 'Cars 2', '2017', '3', 'Acción', 'La historia de como Rayo McQueen consigue hacerse con la copa pistón');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('39002319', 'Los 5 justos otra vez', '2006', '1', 'Fantasía', '3 hermanos, su prima y un perro se reunen de nuevo en verano para vivir aventuras inimaginables');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('33404512', 'Regreso al futuro', '1999', '5', 'Ciencia y Ficción', 'Narra el proceso por el que pasa el protagonista para conseguir reparar la maquina que habia creado para viajar en el tiempo');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('52179746', 'Geronimo Stilton', '2011', '8', 'Fantasía', 'Cuenta las aventuras de los sueños de un raton, en los que se adentra en mundos magicos con todo tipo de criaturas');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('26609934', 'Codigo Lyoko', '2008', '3', 'Fantasia', 'Un grupo de amigos descubren un ordenador muy especial en el que encuentran un nuevo mundo virtual llamado Lyoko, ocupado por un virus y una joven de su edad');
+INSERT INTO `bibliotecas_ay_pamplona`.`libros` (`isbn`, `titulo`, `anyo`, `numero_edicion`, `genero`, `descripcion`) VALUES ('20915662', 'Bajo la misma estrella', '2018', '11', 'Amor', 'Historia de amor en la que se hace incapie en la valoracion del tiempo de nuestra vida');
 
 /*CONSULTAS: Nota: Se incluirá como comentario el enunciado de cada consulta.*/
 
@@ -149,15 +149,15 @@ de formato: “Nombre: L. Dorado”.
 Pista: los apellidos pueden estar separadas o por un espacio o por un guion.*/
 SELECT CONCAT(LEFT(nombre,1),".", " ",apellidos) as "Nombre Completo"
 from autor
-where fecha_muerte - fecha_nacimiento > 30
+where fecha_muerte - fecha_nacimiento > 30;
 
 /* Ejercicio 5: Lista de países en mayúsculas con el número de escritores de cada país
-para países que tengan más de un escritor. Ten en cuenta solo los escritores
-vivos. */
+para países que tengan más de un escritor. Ten en cuenta solo los escritores */
 select UPPER(pais_origen), count(pais_origen)
 from autor
-where (count(pais_origen) > 1) AND (fecha_muerte IS NULL)
-group by pais_origen;
+where (fecha_muerte IS NULL)
+group by pais_origen 
+having count(pais_origen) > 1;
 
 
 /* Ejercicio 6: Muestra la duración media en horas de las reservas de espacios por cada
@@ -177,8 +177,9 @@ descripciones: Z".
 Ordénalas según la suma del tamaño de descripción de cada tipo de más
 larga a menos larga. */
 
-select genero, count(*), sum(char_length(descripcion))
+select genero, count(genero), sum(char_length(descripcion))
 from libros
+group by genero
 order by sum(char_length(descripcion)) desc;
 
 /*PROCEDIMIENTOS ALMACENADOS (RA5)
@@ -188,8 +189,8 @@ Notas:
 - Todos los procedimientos, funciones, triggers, etc. deberán contar con sentencias (CALL, etc.) que permitan comprobar su funcionamiento.*/
 
 -- Crea un procedimiento y pruébalo...
-CREATE PROCEDURE...
-CALL ...
+-- CREATE PROCEDURE...
+-- CALL ...
 
 /* Ejercicio 4: Crea un procedimiento que reciba por parámetro un año y calcule el
 número medio, máximo y mínimo de minutos que han durado las reservas
@@ -197,54 +198,20 @@ de espacios en ese año. Insertará esos datos en tres variables para después
 mostrarlos por pantalla de la siguiente manera: “Las reservas de espacios
 en el año W han tenido un tiempo mínimo de X minutos, un tiempo máximo
 de Y minutos y un tiempo medio de Z minutos”. */ 
-DELIMITER $$
-DROP PROCEDURE IF EXISTS reservas_año;
-CREATE PROCEDURE reservas_año (año YEAR)
-BEGIN
-	DECLARE media FLOAT;
-    DECLARE maximo FLOAT;
-    DECLARE minimo FLOAT;
-    DECLARE texto VARCHAR(60);
-    
-    SET media = select AVG(MINUTE(fecha_fin - fecha_inicio)) from reservas 
-    where year(fecha_inicio) == año; 
-    
-     SET maximo = select MAX(MINUTE(fecha_fin - fecha_inicio)) from reservas 
-    where year(fecha_inicio) == año; 
-    
-	 SET minimo = select MIN(MINUTE(fecha_fin - fecha_inicio)) from reservas 
-    where year(fecha_inicio) == año; 
-    
-    SET texto = CONCAT("Las reservas de espacios
-		en el año ", año, " han tenido un tiempo mínimo de ",minimo " minutos, un tiempo máximo
-		de ", maximo, " minutos y un tiempo medio de ", media, " minutos")
-        
-	select texto;
-END
-$$
-DELIMITER;
+
     
 -- Crea una función y pruébala...
-CREATE FUNCTION ...
-SELECT ...
+-- CREATE FUNCTION ...
+-- SELECT ...
 
 /* Ejercicio 2: Escribe una función que reciba como parámetros una fecha de nacimiento
 y un país. La función devolverá la diferencia en años entre la edad media
 de ese país y la edad correspondiente a la fecha de nacimiento pasada
 por parámetro */
-DELIMITER $$
-CREATE FUNCTION diferencia_media (fecha_nacimiento DATE, pais_origen VARCHAR(30), edad_media INT) RETURNS int 
-BEGIN
-	 DECLARE edad_actual int;
-	 SET edad_actual = DATE(NOW()) - fecha_nacimiento;
-     
-     return edad_actual;
-END
-$$
-DELIMITER ;
+
 
 -- Crea un trigger y pruébalo...
-CREATE TRIGGER ...
-INSERT...
+-- CREATE TRIGGER ...
+-- INSERT...
 
 -- ETC.
